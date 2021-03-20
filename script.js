e36278720e5218d452c846c969ebf2035ec93b66
iamquiroz <jquiroz.mexicali@gmail.com>
2021-03-20 15:22:32 -0700

    quiz game commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+const shuffledQuestions, currentQuestionIndex
+startButton.addEventListener('click', startGame);
+function startGame() {
+    console.log('Started');
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer(){
+
+}
+
+const questions = [
+  {
+    question: 'What is 2 + 2',
+    answers: [
+      { text: '4', correct: true},
+      { text: '22', correct: false}
+    ]
+  }
+]
